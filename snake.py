05cbbe02dfdcf9463e1424aeba350d29706a4b48
fiona <fiona.kitchen@yahoo.co.uk>
2023-01-23 14:27:37 +0000

    working with animation and tracer

--- snake.py
+++ snake.py
@@ -4,18 +4,30 @@ screen = Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("Welcome to the Snake Game")
+screen.tracer(0)
 
+game_is_on = True 
+
+starting_positons=[(0,0),(-20,0),(-40,0)]
+
+segments=[]
 
 ## we want to make a snake body - three squares- three sqaures 20px apart
-segment_1 = Turtle(shape="square")
-segment_1.color("white")
 
-segment_2 = Turtle(shape="square")
-segment_2.color("white")
-segment_2.goto(-20,0)
+for position in starting_positons:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
 
-segment_3 = Turtle(shape="square")
-segment_3.color("white")
-segment_3.goto(-40,0)
+screen.update()
+
+game_is_on = True
+
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)    
+
 
 screen.exitonclick()
